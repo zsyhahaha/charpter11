a926f6d1ef2b164a02cd9a6589e3efb66e8f8c76
zsyhahaha <55183431+zsyhahaha@users.noreply.github.com>
2022-07-15 14:34:07 +0800

    Add files via upload

---
+++ vector.h
@@ -0,0 +1,60 @@
+//
+// Created by »ªË¶ on 15/7/2022.
+//
+
+#ifndef CHARPTER11_VECTOR_H
+#define CHARPTER11_VECTOR_H
+#include <iostream>
+
+namespace VECTOR{
+    class Vector {
+    public:
+        enum Mode {RECT,POL};
+    private:
+        double x;
+        double y;
+        double mag;
+        double ang;
+        Mode mode;
+        void set_mag();
+        void set_ang();
+        void set_x();
+        void set_y();
+    public:
+        Vector();
+        Vector(double n1,double n2,Mode form = RECT);
+        void reset(double n1,double n2,Mode form = RECT);
+        ~Vector();
+        double xval() const
+        {
+            return x;
+        }
+        double yval() const
+        {
+            return y;
+        }
+        double magval() const
+        {
+            return mag;
+        }
+        double angval() const
+        {
+            return ang;
+        }
+        void polar_mode();
+        void rect_mode();
+        //operator overloading
+        Vector operator+(const Vector & b) const;
+        Vector operator-(const Vector & b) const;
+        Vector operator-() const;
+        Vector operator*(double n) const;
+        //friends
+        friend Vector operator*(double n,const Vector & a);
+        friend std::ostream & operator<<(std::ostream & os,const Vector & v);
+
+    };
+
+}
+
+
+#endif //CHARPTER11_VECTOR_H
